439c215d0a53915c022ae3d973dcaeab7f3ebe09
elgit0 <eldwinadhichandra@gmail.com>
2024-06-15 18:29:34 +0700

    fodocku

--- view.py
+++ view.py
@@ -1,13 +1,36 @@
 from flask import Flask, render_template
 import os
 
+import matplotlib
+matplotlib.use('Agg')
+import statsmodels.api as sm
+import pandas as pd
+import matplotlib.pyplot as plt
+
 app = Flask(__name__, template_folder='template')
 
-
 @app.route('/')
+
 def home():
     return render_template('index.html')
 
+def index():
+    data = pd.read_csv('AirPassengers.csv')
+    data['Month'] = pd.to_datetime(data['Month'])
+
+    data.columns = ['Month','Passengers']
+    data['Month'] = pd.to_datetime(data['Month'], format='%Y-%m')
+    data = data.set_index('Month')
+    data.head(12)
+    decomposition = sm.tsa.seasonal_decompose(data.Passengers, model='multiplicative')
+    a = decomposition.seasonal
+    a.plot()
+    try:
+        os.remove('foo.png')
+    except OSError:
+        pass
+    plt.savefig('foo.png')
+    return "Hello!"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3000))
